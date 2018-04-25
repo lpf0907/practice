e848e4f44be00b1264bff76db29a9ce3b60c4a91
lpf0907 <lpf0907@gmail.com>
2018-04-26 00:30:18 +0800

    Q1004

---
+++ src/main/java/algorithm/poj/Q1004.java
@@ -0,0 +1,21 @@
+package algorithm.poj;
+
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Q1004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        float total = 0;
+        float money = 0;
+        while (i<12) {
+            money = sc.nextFloat();
+            total+=money;
+            i++;
+        }
+        float average = total/12;
+        String  result = new Formatter().format("%.2f", average).toString();
+        System.out.println("$"+result);
+    }
+}
