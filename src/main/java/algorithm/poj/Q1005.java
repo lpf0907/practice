4fb401432146bb8d12ee875c841032fad3e00ab7
lpf0907@gmail.com <MypasswordL7>
2018-10-12 18:00:37 +0800

    update 1005

--- src/main/java/algorithm/poj/Q1005.java
+++ src/main/java/algorithm/poj/Q1005.java
@@ -8,18 +8,20 @@ public class Q1005 {
         int N = sc.nextInt() * 2;
         float[] a = new float[N];
         int m = 0;
-        while(sc.hasNext()){
+        while(m<N){
             a[m] = sc.nextFloat();
             m++;
         }
-        float[] squar = new float[N/2];
+        float[] squar = new float[N];
+        int j = 0;
         for (int i = 0;i<N-1;i=i+2){
-            int j = 0;
-            squar[j] = a[i] * a[i+1] * (float) Math.PI/2;
+            squar[j] = (a[i] * a[i] + a[i+1]*a[i+1])* (float) Math.PI/2;
+            j++;
         }
         for (int k = 0; k<N/2;k++){
             int tmp = (int)Math.ceil(squar[k]/50);
-            System.out.println("Property "+k+": This property will begin eroding in year "+tmp+".");
+            int tmp2 = k+1;
+            System.out.println("Property "+tmp2+": This property will begin eroding in year "+tmp+".");
         }
         System.out.println("END OF OUTPUT.");
     }
