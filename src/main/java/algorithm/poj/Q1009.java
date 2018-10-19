d95b2da52bc96b3a89b0becc315e8706483300cd
lpf0907@gmail.com <MypasswordL7>
2018-10-19 18:11:30 +0800

    Q1009 add

---
+++ src/main/java/algorithm/poj/Q1009.java
@@ -0,0 +1,12 @@
+package algorithm.poj;
+
+import java.util.Scanner;
+
+public class Q1009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            
+        }
+    }
+}
