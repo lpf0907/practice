ca041807ae8fbe80e5a58b4d759a0cd113d999f5
lpf0907@gmail.com <MypasswordL7>
2019-05-10 21:32:19 +0800

    Q1009 update and test update

--- src/main/java/algorithm/poj/Q1009.java
+++ src/main/java/algorithm/poj/Q1009.java
@@ -6,7 +6,14 @@ public class Q1009 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            
+            String str = sc.nextLine();
+            String[] input = str.split(" ");
+            //输入处理
+            if (input.length == 1 && Integer.parseInt(input[0]) == 0){
+                break;
+            }else {
+
+            }
         }
     }
 }
--- src/main/java/algorithm/test.java
+++ src/main/java/algorithm/test.java
@@ -1,26 +1,17 @@
 package algorithm;
 
-import java.util.Scanner;
 
 public class test {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        while(true)
-        {
-            if(sc.hasNextInt())
-            {
-                int result=1;
-                int n=sc.nextInt();
-                for(int i =1;i<=n;i++)
-                    result*=i;
-                System.out.println(result);
-            }
-            else
-            {
-                break;
-            }
-        }
-        sc.close();
+        double a = 10.2;
+        double b = 10.19;
+
+
+        double c = 5;
+        double result1 = a-c;
+        double result2 = b-c;
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
