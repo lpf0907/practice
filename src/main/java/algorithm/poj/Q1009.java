ebae96719051c303276f8bb6606e7305917f7d21
Pengfei Li <lpf0907@gmail.com>
2019-05-12 15:47:06 +0000

    Update Q1009.java
    update

--- src/main/java/algorithm/poj/Q1009.java
+++ src/main/java/algorithm/poj/Q1009.java
@@ -1,19 +1,43 @@
 package algorithm.poj;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Q1009 {
     public static void main(String[] args) {
+        // 数据属性
+        int length = 0;
+        ArrayList<Integer> mapNum = new ArrayList<Integer>();
+        Integer[][] map;
+        //输入
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
             String str = sc.nextLine();
             String[] input = str.split(" ");
             //输入处理
-            if (input.length == 1 && Integer.parseInt(input[0]) == 0){
-                break;
+            if (input.length == 1 ) {
+                //处理长度、结束
+                if( Integer.parseInt(input[0]) == 0){
+                    break;
+                }else {
+                    length =Integer.parseInt(input[0]);
+                }
             }else {
-
+                //录入图像数据
+                if (Integer.parseInt(input[0]) == 0 && Integer.parseInt(input[1]) == 0){
+                    mapNum = new ArrayList<Integer>();
+                }else {
+                    for (int i = 0 ; i<Integer.parseInt(input[1]);i++){
+                        mapNum.add(Integer.parseInt(input[1]));
+                    }
+                    handle(mapNum,length);
+                }
             }
         }
+    }
+
+    //图像处理
+    private static void handle(ArrayList<Integer> mapNum, int length){
+        System.out.println(mapNum);
     }
 }
